61fc371f505256bc2e01981e128679482267e098
Rishabh987654321 <rishabhkumar90063@gmail.com>
2025-04-22 00:30:47 +0530

    package json and app.jsx with some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Hero from './sections/Hero'
+import ShowcaseSection from './sections/ShowcaseSection'
+import Navbar from './components/Navbar'
+import LogoSection from './components/LogoSection'
+import FeatureCards from './sections/FeatureCards'
+import ExperienceSection from './sections/ExperienceSection'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Navbar/>
+      <Hero/>
+      <ShowcaseSection/>
+      <LogoSection/>
+      <FeatureCards/>
+      <ExperienceSection/>
+    </>
   )
 }
 
